724ba22b3d2c2d96613970e198c359172f540cea
Het Patel <hackair@192.168.1.3>
2024-10-15 20:53:47 +0530

    Auto-commit on 2024-10-15 20:53:47

--- auto_push.sh
+++ auto_push.sh
@@ -4,7 +4,7 @@ while true; do
     # Check for changes
     git add .
     git commit -m "Auto-commit on $(date +"%Y-%m-%d %H:%M:%S")" --allow-empty
-    git push origin main # Change 'main' to your branch name if different
+    git push origin master # Change 'main' to your branch name if different
     
     # Sleep for a specified interval (e.g., 60 seconds)
     sleep 60
